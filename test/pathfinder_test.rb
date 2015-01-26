b5c7493e76222357d0b5e3b83c6caa8880603a4a
pote <pote@tardis.com.uy>
2015-01-25 23:38:12 -0200

    Use yaml in tests

--- test/pathfinder_test.rb
+++ test/pathfinder_test.rb
@@ -1,17 +1,16 @@
 require File.expand_path("../lib/wayfinder", File.dirname(__FILE__))
+require 'yaml'
 
 test 'load basic ability scores' do
-  main = {
-    ability_scores: {
-      'strength'     => 16,
-      'dexterety'    => 14,
-      'constitution' => 14,
-      'intelligence' => 12,
-      'wisdom'       => 8,
-      'charisma'     => 16
-    }
-  }
-
+  main = YAML.load("
+ability_scores:
+  strength: 16
+  dexterity: 14
+  constitution: 14
+  intelligence: 12
+  wisdom: 8
+  charisma: 16
+  ")
 
   c = Wayfinder::Character.new({
     main: main,
